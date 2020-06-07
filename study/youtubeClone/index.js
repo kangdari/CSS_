884f636b83ed37b8c55414acf7ae5473688a2a5e
kangdari_ <ksh44820@naver.com>
2020-06-07 23:15:30 +0900

    youtube clone - create sideBar

--- study/youtubeClone/index.js
+++ study/youtubeClone/index.js
@@ -1,9 +1,9 @@
 // header event
 const $header__center = document.querySelector(".header__center");
-const $search_icon = document.querySelector(".search_icon");
+const $search__box = document.querySelector(".search__box");
 const $back_btn = document.querySelector(".back_btn");
 
-$search_icon.addEventListener("click", () => {
+$search__box.addEventListener("click", () => {
   $header__center.classList.toggle("visible");
 });
 
@@ -16,6 +16,7 @@ const $aside_btn = document.getElementsByClassName("aside__btn");
 const $side__container = document.querySelector(".side__container");
 const $side__background = document.querySelector(".side__background");
 
+// side open or close
 const side_show = () => {
   $side__container.classList.toggle("show");
   $side__background.classList.toggle("show");
@@ -27,6 +28,7 @@ const side_show = () => {
 
 $side__background.addEventListener("click", side_show);
 
+// 더 보기
 const $expansion__btn = document.querySelector(".expansion__btn");
 const $side__menu__expansion = document.querySelector(".side__menu__expansion");
 const $simple__btn = document.querySelector(".simple__btn");
@@ -39,4 +41,19 @@ $expansion__btn.addEventListener("click", () => {
 $simple__btn.addEventListener("click", () => {
   $side__menu__expansion.classList.toggle("show");
   $expansion__btn.style.display = "flex";
+});
+
+//  구독 더보기
+const $sub__expansion__btn = document.querySelector('.sub__expansion__btn');
+const $sub__side__menu__expansion = document.querySelector('.sub__side__menu__expansion');
+const $sub__simple__btn = document.querySelector('.sub__simple__btn');
+
+$sub__expansion__btn.addEventListener("click", () => {
+  $sub__side__menu__expansion.classList.toggle("show");
+  $sub__expansion__btn.style.display = "none";
+});
+
+$sub__simple__btn.addEventListener("click", () => {
+  $sub__side__menu__expansion.classList.toggle("show");
+  $sub__expansion__btn.style.display = "flex";
 });
